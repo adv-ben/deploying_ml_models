3753962e66d5070747790cd2597e21d21ded7a22
bea-3ce <bea-3ce@github.com>
2023-10-04 13:33:36 +0800

    A

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
-"""
-import streamlit as st
-import torch
-import torch.nn as nn
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-torch.manual_seed(31337)
-
-st.title("Lorem Ipsum")
-st.text("Lorem ipsum dolor sit amet")
-
-st.header("Dummy model")
-num = st.number_input("Input a number and it will be doubled")
-butt = st.button("Test button")
-
-model = nn.Linear(1, 1)
-
-#return value in textbox when button pressed
-if butt:
-    val = model(torch.tensor([num]))
-    st.text("Result: " + str(val.item()))
-"""
-
+# [START gae_python38_app]
+# [START gae_python3_app]
 from flask import Flask
 
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
+    """Return a friendly HTTP greeting.
+
+    Returns:
+        A string with the words 'Hello World!'.
+    """
     return "Hello World!"
+
 
 if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. You
     # can configure startup instructions by adding `entrypoint` to app.yaml.
     app.run(host="127.0.0.1", port=8080, debug=True)
-
-
+# [END gae_python3_app]
+# [END gae_python38_app]
